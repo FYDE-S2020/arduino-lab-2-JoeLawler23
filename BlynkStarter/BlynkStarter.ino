4047dea84087f8ae3dc9362aea95cc9efe92564d
“joseph <josephlawler23@utexas.edu>
2020-03-08 17:13:48 -0500

    Final

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -1,36 +1,30 @@
 /*************************************************************
   Download latest Blynk library here:
     https://github.com/blynkkk/blynk-library/releases/latest
-
   Blynk is a platform with iOS and Android apps to control
   Arduino, Raspberry Pi and the likes over the Internet.
   You can easily build graphic interfaces for all your
   projects by simply dragging and dropping widgets.
-
     Downloads, docs, tutorials: http://www.blynk.cc
     Sketch generator:           http://examples.blynk.cc
     Blynk community:            http://community.blynk.cc
     Follow us:                  http://www.fb.com/blynkapp
                                 http://twitter.com/blynk_app
-
   Blynk library is licensed under MIT license
   This example code is in public domain.
-
  *************************************************************
   This example runs directly on ESP32 chip.
-
   Note: This requires ESP32 support package:
     https://github.com/espressif/arduino-esp32
-
   Please be sure to select the right ESP32 module
   in the Tools -> Board menu!
-
   Change WiFi ssid, pass, and Blynk auth token to run :)
   Feel free to apply it to any other example. It's simple!
  *************************************************************/
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
+#define LED 2
 
 
 #include <WiFi.h>
@@ -39,24 +33,83 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "_y2MStxNCmQiCaU6ZcnsGnk-HvCD_ria";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "utguest";
+char pass[32] = "";
+
+const int ledChannel = 0;
+const int freq = 5000;
+const int resolution = 10;
+
+BlynkTimer timer;
+
+int time_count = 0; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+
+void myTimerEvent() // Every 10 ms
+{
+    if (time_count % 25 == 0) { // every 250 ms
+        // Do thing that needs to happen every 0.25 seconds
+    }
+    if (time_count == 100){
+        Blynk.virtualWrite(V3, millis() / 1000); // Write the arduino uptime every second
+        time_count = 0; // reset time counter
+    }
+    else {
+        // Send serial data to Blynk terminal
+        char character;
+        while(Serial.available()) { // Check if serial is available every 10 ms
+            character = Serial.read();
+            content.concat(character);
+        }
+        if (content != "") {
+            Blynk.virtualWrite(V4, content);
+            content = ""; // Clear String
+        }  
+    }
+    time_count += 1; // Increment on every tick
+}
 
 void setup()
 {
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(LED, ledChannel);
+
+  // Setup a function to be called every second
+  timer.setInterval(10L, myTimerEvent);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
 
+BLYNK_WRITE(V0)
+{
+  int pinValue = param.asInt();
+
+  if(pinValue == 0)
+  {
+     ledcWrite(ledChannel, 0);
+  }
+  else
+  {
+     ledcWrite(ledChannel, 1023);
+  }
+}
+
+BLYNK_WRITE(V2)
+{
+  int pinValue = param.asInt();
+  ledcWrite(ledChannel, pinValue);
+
+}
